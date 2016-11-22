a6a1e3ecc007e65cd43053c5999822fab1ac9e51
isabellechalhoub <isabelle.chalhoub@gmail.com>
2016-11-22 10:48:01 -0700

    first commit
    hopefully this works

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public float fallDelay;
+
+    private BoxCollider2D player;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (player.IsTouching(gameObject.GetComponent<BoxCollider2D>()))
+        {
+            StartCoroutine(Fall());
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(fallDelay);
+        rb2d.isKinematic = false;
+        yield return 0;
+    }
+}
